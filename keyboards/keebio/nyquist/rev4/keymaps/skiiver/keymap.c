84b45ab9c5450b3c6f159279045c2083e61f6dcc
Steve <steve.kuchta@gmail.com>
2024-01-31 04:03:19 -0700

    split tweaks

--- keyboards/keebio/nyquist/rev4/keymaps/skiiver/keymap.c
+++ keyboards/keebio/nyquist/rev4/keymaps/skiiver/keymap.c
@@ -40,8 +40,8 @@ enum layer_names {
 #define CMMTB A(G(KC_SLSH)) // Toggle Block comment
 
 // Browser Nav
-#define NTAB A(G(KC_RGHT)) 
-#define PTAB A(G(KC_LEFT))
+#define NXTTAB A(G(KC_RGHT)) 
+#define PRETAB A(G(KC_LEFT))
 #define WFWD G(KC_RBRC) 
 #define WBAK G(KC_LBRC)
 
@@ -69,7 +69,9 @@ enum layer_names {
 #define HYPDEL  HYPR_T(KC_DEL)  //Hyper (Left Control, Shift, Alt and GUI)
 #define MEHESC  MEH_T(KC_ESC)  //Meh (Left Control, Shift, and Alt)
 #define CAGGRV  LCAG_T(KC_GRV)
-#define RS_ENT  LSFT_T(KC_ENT)   
+#define RS_ENT  LSFT_T(KC_ENT)  
+#define LS_ENT  LSFT_T(KC_ENT)  
+//#define LS_ENT  MT(MOD_LSFT, KC_ENT) 
 
 // Number Row
 #define PR_1 C_S_T(KC_1)
@@ -90,17 +92,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty*/
 [_QWERTY] = LAYOUT_ortho_5x12(
-CAGGRV,  PR_1,    PR_2,    PR_3,    PR_4,  PR_5,   PR_6,   PR_7,  PR_8,    PR_9,    PR_0,     HYPDEL,
-HYPTAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPC,
-MEHESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  RS_ENT,
-LFN,     KC_LCTL, KC_LALT, KC_LGUI, LOWER, LOWER,  RAISE,  RAISE, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
+CAGGRV,  PR_1,    PR_2,    PR_3,    PR_4,  PR_5,   PR_6,   PR_7,  PR_8,    PR_9,    PR_0,    HYPDEL,
+HYPTAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+MEHESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+LS_ENT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  RS_ENT,
+LFN,     KC_LCTL, KC_LALT, KC_LGUI, LOWER, LOWER,  RAISE,  RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower */
 [_LOWER] = LAYOUT_ortho_5x12(
 _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,   _______,
-KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,   KC_RPRN,  KC_BSPC,
+_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,   KC_RPRN,  _______,
 _______, ES_EXC,  KC_NUBS, ES_QUE,  EU_EURO, KC_NDSH, KC_MDSH, KC_UNDS, KC_PLUS, KC_LBRC,   KC_RBRC,  KC_PIPE,
 _______, UNI_BANG,SC_DEG,  KC_APPL, EU_GBP,  LGUIL,   RGUIL,   KC_MINS, KC_EQL,  G(KC_DOWN),G(KC_UP), _______,
 _______, _______, _______, _______, _______, _______, _______, _______, OUTDENT, KC_PGDN,   KC_PGUP,  INDENT
@@ -108,11 +110,11 @@ _______, _______, _______, _______, _______, _______, _______, _______, OUTDENT,
 
 /* Raise */
 [_RAISE] = LAYOUT_ortho_5x12(
-_______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  ___x___, _______,
-KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, KC_NUBS, KC_LCBR, KC_RCBR, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, PTAB,    WBAK,    WFWD,    NTAB
+_______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_GRV, _______,
+_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+_______, _______, _______, _______, _______, _______, _______, _______, KC_NUBS, KC_LCBR, KC_RCBR, _______,
+_______, _______, _______, _______, _______, _______, _______, _______, PRETAB,  WBAK,    WFWD,    NXTTAB
 ),
 
 /* FUNCTIONs */
